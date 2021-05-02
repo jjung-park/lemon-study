0ed14727f6aa2f9f28e427ce05c9ed4bb77df66a
jungnii <jj112490@gmail.com>
2021-05-02 17:03:12 +0900

    webpack setting

---
+++ basic/NumberBaseball.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>{{result}}</h1>
+        <form v-on:submit="onSubmitForm">
+            <input type="text" ref="" maxlength="4" v-model="value">
+            <button>입력</button>
+        </form>
+        <div>
+            시도: {{}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            value:'',
+        }
+    },
+    methods:{
+        onSubmitForm:function(e){
+            e.preventDefault()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- basic/webpack.config.js
+++ basic/webpack.config.js
@@ -1,3 +1,26 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
 module.exports = {
-    
+    mode:'development',
+    devtool:'eval',
+    resolve:{
+        extensions:['.js','.vue'],
+    },
+    entry:{
+        app:path.join(__dirname, 'main.js') //app : 하나로 합쳐질 파일의 이름
+    },
+    module:{
+        rules:[{
+            test:/\.vue$/,
+            loader:'vue-loader',
+        }]
+    },
+    plugins:[
+        new VueLoaderPlugin()
+    ],
+    output:{
+        filename:'[name].js',
+        path:path.join(__dirname, 'dist')
+    },
 }
