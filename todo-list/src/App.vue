697935631850a224565719e13b1bdf4f91b0f6aa
jungnii <jj112490@gmail.com>
2021-05-02 13:05:58 +0900

    컴포넌트 생성

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Title from './components/Title.vue'
+import Input from './components/Input.vue'
+import Controller from './components/Controller.vue'
+import List from './components/List.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Title,
+    Input,
+    Controller,
+    List,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
