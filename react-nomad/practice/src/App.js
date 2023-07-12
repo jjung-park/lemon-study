38f264ec674adf6fcdc2f044615461db1a2bf370
jjung-park <jj112490@gmail.com>
2023-07-12 18:32:24 +0900

    router

--- react-nomad/practice/src/App.js
+++ react-nomad/practice/src/App.js
@@ -1,45 +1,20 @@
-import {useState, useEffect} from 'react';
+import Movie from './components/Movie';
+import Home from './routes/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
-      ).json()
-    /* == 
-    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`);
-    const json = await response.json();*/
-    //async-await == .then()이랑 같음
-    
-    setMovies(json.data.movies);
-    setLoading(false)
-    
-  }
-  useEffect(()=>{
-    getMovies()
-    
-  },[]);
+  return <Router>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
 
-  return (
-      <div>
-        {loading ? (<h1>Loading..</h1>) : (
-        <div>
-          {movies.map((movie) => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image}/>
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <ul>
-              <li>{movie.genres.map(v => <li key={v}>{v}</li>)}</li>
-            </ul>
-          </div>
-          ))}
-        </div>
-        )}
-        
-      </div>
-  );
+    </Switch>
+  </Router>
 }
 
 export default App;
