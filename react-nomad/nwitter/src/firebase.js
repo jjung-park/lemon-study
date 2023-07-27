1673567169e811161daa12be304bb76140afc717
jung-park <jj112490@gmail.com>
2023-07-27 20:38:49 +0900

    Router 생성

---
+++ react-nomad/nwitter/src/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from "firebase/app"
+const firebaseConfig = {
+    apiKey: "AIzaSyDMWWNA-XUKtdrx9H9mKqtuKxs2a-3Ocso",
+    authDomain: "nwitter-26c67.firebaseapp.com",
+    projectId: "nwitter-26c67",
+    storageBucket: "nwitter-26c67.appspot.com",
+    messagingSenderId: "936832823920",
+    appId: "1:936832823920:web:9e356b5a65453fe0e87ab5"
+};
+
+export default firebase.initializeApp(firebaseConfig);
